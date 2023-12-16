35dd692b7a5be26f3e2ab20f5195f37e318cc3a7
prata0x <prata0x@gmail.com>
2023-12-17 05:06:14 +0900

    Fix Physics 2D support

--- Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableJointTrigger.cs
+++ Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableJointTrigger.cs
@@ -1,4 +1,4 @@
-﻿#if !UNITY_2019_1_OR_NEWER || UNIRX_PHYSICS_SUPPORT
+﻿#if !UNITY_2019_1_OR_NEWER || UNIRX_PHYSICS_SUPPORT || UNIRX_PHYSICS2D_SUPPORT
 
 using System; // require keep for Windows Universal App
 using UnityEngine;
@@ -8,6 +8,7 @@ namespace UniRx.Triggers
     [DisallowMultipleComponent]
     public class ObservableJointTrigger : ObservableTriggerBase
     {
+#if !UNITY_2019_1_OR_NEWER || UNIRX_PHYSICS_SUPPORT
         Subject<float> onJointBreak;
 
         void OnJointBreak(float breakForce)
@@ -19,8 +20,9 @@ namespace UniRx.Triggers
         {
             return onJointBreak ?? (onJointBreak = new Subject<float>());
         }
-        
-        
+#endif
+
+#if !UNITY_2019_1_OR_NEWER || UNIRX_PHYSICS2D_SUPPORT
         Subject<Joint2D> onJointBreak2D;
 
         void OnJointBreak2D(Joint2D brokenJoint)
@@ -32,18 +34,23 @@ namespace UniRx.Triggers
         {
             return onJointBreak2D ?? (onJointBreak2D = new Subject<Joint2D>());
         }
-        
+#endif
 
         protected override void RaiseOnCompletedOnDestroy()
         {
+#if !UNITY_2019_1_OR_NEWER || UNIRX_PHYSICS_SUPPORT
             if (onJointBreak != null)
             {
                 onJointBreak.OnCompleted();
             }
+#endif
+
+#if !UNITY_2019_1_OR_NEWER || UNIRX_PHYSICS2D_SUPPORT
             if (onJointBreak2D != null)
             {
                 onJointBreak2D.OnCompleted();
             }
+#endif
         }
     }
 }
